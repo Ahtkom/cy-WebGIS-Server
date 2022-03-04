b41858649c0cde9bfc2e2a41a995653c45a24142
Ahtkom <1459wzls250@gmail.com>
2022-03-04 17:49:17 +0800

    Initial Commit

--- onlywebStartup.sh
+++
@@ -1,9 +0,0 @@
-# cd /home/Ahtkom/pmk/project/cy-WebGIS-Server/webgis/map/
-# npm run build
-
-# cd /home/Ahtkom/pmk/project/cy-WebGIS-Server/webgis/
-rm -rf /opt/tomcat/latest/webapps/webgis/map
-rm -f /opt/tomcat/latest/webapps/webgis/index.html
-
-cp -r /home/Ahtkom/pmk/project/cy-WebGIS-Server/webgis/map/ /opt/tomcat/latest/webapps/webgis/
-cp /home/Ahtkom/pmk/project/cy-WebGIS-Server/webgis/index.html /opt/tomcat/latest/webapps/webgis/
